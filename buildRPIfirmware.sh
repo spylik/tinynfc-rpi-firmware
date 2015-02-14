9f95367a97f496190bd45354820acfbba074f798
Oleksii Semilietov <spylik@gmail.com>
2015-02-14 12:23:45 +0000

    3.18.7+

--- buildRPIfirmware.sh
+++ buildRPIfirmware.sh
@@ -2,14 +2,13 @@
 
 cd /usr/src/raspberry/linux
 
-#building config
 #make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bcmrpi_defconfig
 
 #building all
-#make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-
 
 #copy kernel to tinynfc-rpi-firmware folder
-cp /usr/src/raspberry/linux/arch/arm/boot/Image /usr/src/raspberry/tinynfc-rpi-firmware
+cp /usr/src/raspberry/linux/arch/arm/boot/Image /usr/src/raspberry/tinynfc-rpi-firmware/
 
 # building modules
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH=/usr/src/raspberry/tinynfc-rpi-firmware modules
